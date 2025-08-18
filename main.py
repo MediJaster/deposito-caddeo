916451deafd06962a895de3712dd9f8e582bb0ef
Giorgio Caddeo <medijaster@protonmail.com>
2025-08-18 14:54:44 +0200

    test: test github actions

---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    print("Hello from deposito-caddeo!")
+
+
+    test ={}
+
+    test=test
+
+    test = {
+
+
+
+
+        "test":"test github actions"
+    }
+
+    print(test
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_prova.py
@@ -0,0 +1,2 @@
+def test_prova():
+    assert 1 + 1 == 2
