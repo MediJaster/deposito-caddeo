ca681fdc39be08906fe73f24ac39cba2b4fd49b9
Giorgio Caddeo <medijaster@protonmail.com>
2025-08-19 10:56:25 +0200

    feat: scansione cartella dataset e funzione per caricamento csv con pandas

--- 19-08/hourly-energy-consumption/main.py
+++ 19-08/hourly-energy-consumption/main.py
@@ -1,4 +1,46 @@
+import os
+
+import pandas as pd
+
+DATASET_FOLDER = os.path.join(os.path.dirname(__file__), "datasets")
+COMPANY_LIST = [
+    entry.name.split("_")[0]
+    for entry in os.scandir(DATASET_FOLDER)
+    if entry.is_file() and entry.name.endswith("_hourly.csv")
+]
+
+
+def get_dataset(company: str) -> pd.DataFrame:
+    """Get the dataset for a specific company.
+
+    Parameters
+    ----------
+    company : str
+        The name of the company for which to retrieve the dataset.
+
+    Returns
+    -------
+    pd.DataFrame
+        The dataset containing hourly energy consumption data for the specified company.
+    """
+
+    if company not in COMPANY_LIST:
+        raise ValueError(f"No dataset available for Company '{company}'.")
+
+    filename = f"{company}_hourly.csv"
+
+    return pd.read_csv(
+        os.path.join(DATASET_FOLDER, filename),
+        parse_dates=["Datetime"],
+        index_col="Datetime",
+    )
+
+
 def main() -> None:
+    hourly_consumption_df = get_dataset("COMED")
+
+    print(hourly_consumption_df.head())
+
     return
 
 
